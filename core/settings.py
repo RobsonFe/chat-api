748ab9d941b2ce075c81dad7a81b60c7f8392067
RobsonFe <robson12ferreira@gmail.com>
2025-07-28 15:24:10 -0300

    Adiciona modelo de usuário personalizado e configurações iniciais da aplicação accounts

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Internationalization
