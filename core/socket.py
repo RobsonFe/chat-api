7c1d563dfa7a10a0a1150dc37e0393fe9e2b0fb3
RobsonFe <robson12ferreira@gmail.com>
2025-07-28 15:01:14 -0300

    Altera o modo assíncrono do socket.io para 'eventlet' no arquivo socket.py

--- core/socket.py
+++ core/socket.py
@@ -1,4 +1,4 @@
 import socketio 
 from django.conf import settings
 
-socket = socketio.Server(async_mode='threading', cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS)
+socket = socketio.Server(async_mode='eventlet', cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS)
