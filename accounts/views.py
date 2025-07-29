5122a339101adde5d471d51cb175cf3f5064c2ea
RobsonFe <robson12ferreira@gmail.com>
2025-07-29 10:57:44 -0300

    Aprimora o tratamento de erros na classe SignInView e adiciona a classe SignUpView com validação de campos obrigatórios.

--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,13 @@ from django.core.files.storage import FileSystemStorage
 from rest_framework.permissions import AllowAny
 from accounts.serializers import UserSerializer
 from rest_framework.response import Response
+from core.exceptions import ValidationError
 from rest_framework.views import APIView
 from accounts.auth import Authentication
 from django.utils.timezone import now
 from rest_framework import status
 from accounts.models import User
 from django.conf import settings
-from core.exceptions import ValidationError
 
 
 class SignInView(APIView, Authentication):
@@ -25,7 +25,7 @@ class SignInView(APIView, Authentication):
         signin = self.signin(email, password)
         
         if not signin:
-            raise AuthenticationFailed('Credenciais inválidas.')
+            raise AuthenticationFailed('Credenciais inválidas.', code=status.HTTP_401_UNAUTHORIZED)
           
         user = UserSerializer(signin).data
         refresh = RefreshToken.for_user(signin)
@@ -34,4 +34,31 @@ class SignInView(APIView, Authentication):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
             'user': user,
+        }, status=status.HTTP_200_OK)
+        
+class SignUpView(APIView, Authentication):
+  
+    permission_classes = [AllowAny]
+    authentication_classes = [Authentication]
+
+    def post(self, request):
+
+        name = request.data.get('name')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        if not name or not email or not password:
+            raise ValidationError('Todos os campos são obrigatórios.', code=status.HTTP_400_BAD_REQUEST)
+        
+        singup = self.signup(name, email, password)
+        
+        if not singup:
+            raise AuthenticationFailed('Erro ao registrar.', code= status.HTTP_400_BAD_REQUEST)
+          
+        user = UserSerializer(singup).data
+        refresh = RefreshToken.for_user(singup)
+        
+        return Response({
+            'user': user,
+            'access': str(refresh.access_token),
         }, status=status.HTTP_200_OK)
