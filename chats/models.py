8d20abfe6e7d1bc9eeee237eb39b5bbc4df53d02
RobsonFe <robson12ferreira@gmail.com>
2025-07-29 16:37:59 -0300

    Adiciona a aplicação do módulo de chats com modelos, migrações, admin e configurações iniciais

---
+++ chats/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from accounts.models import User
+
+class Chat(models.Model):
+    from_user = models.ForeignKey(User, related_name='chats_from_user_id', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='chats_to_user_id', on_delete=models.CASCADE)
+    viewed_at = models.DateTimeField(null=True, blank=True)
+    delete_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+      db_table = 'chats'
+      
+      
+class ChatMessage(models.Model):
+
+    body = models.TextField(null=True, blank=True)
+    attachment_code = models.CharField(
+      choices=[
+        ("FILE", "FILE"),('AUDIO', 'AUDIO'),
+        ],
+      max_length=10, null=True, blank=True
+    )
+    attachment_id = models.IntegerField(null=True, blank=True)
+    viewed_at = models.DateTimeField(null=True, blank=True)
+    delete_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
+    from_user = models.ForeignKey(User, related_name='messages_from_user_id', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'chat_messages'
+        ordering = ['-created_at']
