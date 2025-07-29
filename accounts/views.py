aad9980ad01c5975c344f1b902bc82d5450e2f7d
RobsonFe <robson12ferreira@gmail.com>
2025-07-29 10:44:04 -0300

    Implementa a classe SignInView com autenticação e tratamento de erros de credenciais inválidas; adiciona a classe ValidationError para gerenciamento de exceções.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.files.storage import FileSystemStorage
+from rest_framework.permissions import AllowAny
+from accounts.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from accounts.auth import Authentication
+from django.utils.timezone import now
+from rest_framework import status
+from accounts.models import User
+from django.conf import settings
+from core.exceptions import ValidationError
 
-# Create your views here.
+
+class SignInView(APIView, Authentication):
+  
+    permission_classes = [AllowAny]
+    authentication_classes = [Authentication]
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        signin = self.signin(email, password)
+        
+        if not signin:
+            raise AuthenticationFailed('Credenciais inválidas.')
+          
+        user = UserSerializer(signin).data
+        refresh = RefreshToken.for_user(signin)
+        
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'user': user,
+        }, status=status.HTTP_200_OK)
