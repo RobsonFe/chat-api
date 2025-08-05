14631f20059def4cac08a2e0b49eb857bc93949d
RobsonFe <robson12ferreira@gmail.com>
2025-08-05 10:47:25 -0300

    Adiciona lógica para atualizar mensagens como lidas e emitir eventos de atualização via socket.

--- core/socket.py
+++ core/socket.py
@@ -1,4 +1,42 @@
-import socketio 
+import socketio
 from django.conf import settings
+from django.utils.timezone import now
 
-socket = socketio.Server(async_mode='eventlet', cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS)
+from chats.models import Chat, ChatMessage
+
+socket = socketio.Server(
+    async_mode="eventlet", cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS
+)
+
+
+@socket.event
+def update_messages_as_read(sid, data):
+    chat_id = data.get("chat_id")
+
+    chat = Chat.objects.values("from_user_id", "to_user_id").filter(id=chat_id).first()
+
+    ChatMessage.objects.filter(
+        chat_id=chat_id,
+        viewed_at__isnull=True,
+    ).update(
+        viewed_at=now(),
+    )
+
+    socket.emit(
+        "update_chat",
+        {
+            "query": {
+                "users": [chat["from_user_id"], chat["to_user_id"]],
+            }
+        },
+    )
+
+    socket.emit(
+        "mark_messages_as_read",
+        {
+            "query": {
+                "chat_id": chat_id,
+                "exclude_user_id": data.get("exclude_user_id", None),
+            }
+        },
+    )
